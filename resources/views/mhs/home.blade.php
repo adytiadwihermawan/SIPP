433736a8227000399396775729e6b757f54df4f8
ryanramel <1810817110005@mhs.ulm.ac.id>
2021-09-16 12:33:23 +0800

    mamaniskan ui3

--- resources/views/mhs/home.blade.php
+++ resources/views/mhs/home.blade.php
@@ -5,20 +5,22 @@
 @section('content')
     <div class="row">
 	@foreach ($course as $matkul)
-	<div class="col-lg-3 col-6">
+	<div class="col-lg-4">
 		<!-- small box -->
 		
-		<div class="small-box bg-warning">
+		<div class="small-box cold3">
 			<div class="inner">
 			
 				<h5>{{ $matkul->nama_praktikum }}</h5>
 			
 
 				<p>{{$matkul->tahun_ajaran}}</p>
+			
 			</div>
-			<div class="icon">
-				<i class="ion ion-stats-bars"></i>
-			</div>
+			<div class="icon mb-4">
+                <i class="fas fa-desktop"></i>
+              </div>
+		
 			<a href="matkul/{{$matkul->id_praktikum}}" class="small-box-footer">Selengkapnya
 				<i class="fas fa-arrow-circle-right"></i>
 			</a>
