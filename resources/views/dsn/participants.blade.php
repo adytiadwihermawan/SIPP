de8f06b5b15cac256e847a4b690a98f5139cbaf9
ryanramel <1810817110005@mhs.ulm.ac.id>
2021-09-25 23:33:38 +0800

    menambah nama kelas di pasticipant

--- resources/views/dsn/participants.blade.php
+++ resources/views/dsn/participants.blade.php
@@ -3,6 +3,17 @@
 @section('Judul', 'Sistem Informasi Pendataan Praktikum Teknologi Informasi Universitas Lambung Mangkurat')
 
 @section('content')        
+
+<div class="card blue1 ml-3">
+        <div class="card-header">
+            <h3 class="card-title">
+              <b>  {{$mk[0]->nama_praktikum}} </b>
+            </h3>
+
+        </div>
+    </div>
+
+
 <div class="card card-outline card-warning ml-3 py-4 px-4">
 <table style="width: 100%" class="table table-bordered table-striped my-5" id="partisipan">
             <thead>
